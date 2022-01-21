6663148024faae4add6d94900acadbf4ce0f071e
Serga05 <88805832+Serga05@users.noreply.github.com>
2022-01-21 11:55:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function CopyButton() {
+	navigator.clipboard.writeText('steam://install/427920');
+}
+function DocumentSite() {
+	document.write('<a href="serga05.github.io/Fnaf-world"><button class="backbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><h1>Документ сайта</h1><p>Код: 0.710.018.92.0</p><div><body class="js"></body>Адрес: serga05.github.io/Fnaf-world</div><h1>Что это такое?</h1><div class="block2"><text id="white">Это информация для проверки сайтов например: мошшеник копирует сайт, и туда вставляет вредоносные программы, майнер например, и он должен ввести Документ сайта код он может своровать но адрес не может поетому что-бы проверить сайт serga05 зайдите на базу данных и проверьте сайт, И ещё проверяйте адрес который написан <title>fnaf world: Документ сайта</title>странице и на каком вы</text></div>');
+}
